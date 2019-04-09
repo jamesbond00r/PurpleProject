64e2154f5849d8eff7b7ccf27ba415029a3fcd64
jamesbond00r <jamesbond00r@gmail.com>
2019-04-09 16:49:31 +0000

    Add simple React Fourm, Add express wildcard and 404.html

--- router/server.js
+++ router/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const router = express.Router();
 const port = 3001
 
 app.get('/', (req, res) => res.send('Hello Purple wave!'))
+
+
+
+ //Catches all for urls
+app.get('*', (req, res) => res.sendFile(__dirname+ '/404.html'))
+
 
 app.listen(port, () => console.log(`Server has started on ${port}`))
--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 
 class App extends Component {
+ constructor(props){
+   super(props);
+    this.state = {value: ''};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+   }
+    handleChange(event) {
+    this.setState({value: event.target.value});
+   }
+    handleSubmit(event) {
+     alert('The name submitted was: ' + this.state.value);
+    event.preventDefault();
+}
   render() {
     return (
-      <div className="App">
-
-      <h1>Hello Purple Wave!</h1>
-
-<Button variant="primary">Button</Button>
+      <div>
+	<form onSubmit={this.handleSubmit}>
+	 <label>
+		Name:
+	  	<input type="text" value={this.state.value}
+	onChange={this.handleChange} />
+	</label>
+ 	<Button type="submit" value="submit">Submit</Button>
+</form>
 
       </div>
     );
