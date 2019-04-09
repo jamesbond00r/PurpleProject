e7ddb2e0b534be604a7587a8e92fad0a9b883a80
jamesbond00r <jamesbond00r@gmail.com>
2019-04-09 16:56:13 +0000

    added comments

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import Button from 'react-bootstrap/Button';
 class App extends Component {
  constructor(props){
    super(props);
+	//set state value to empty string
     this.state = {value: ''};
-    
+
+    //binds the handleChange and handleSubmit to  the  constructor
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
    }
+
+    //handleChange takes in event from fourm and sets state to
+    //the event value
     handleChange(event) {
     this.setState({value: event.target.value});
    }
+
+    //handleSubmit function  performs acction after submit and 
+    // prevents the auto refresh of the page
     handleSubmit(event) {
      alert('The name submitted was: ' + this.state.value);
     event.preventDefault();
