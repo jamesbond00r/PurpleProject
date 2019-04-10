0d27a0c4dee856bd980b7a83600e3a201aae977a
jamesbond00r <jamesbond00r@gmail.com>
2019-04-10 16:44:25 +0000

    Add Access-Control-Allow-Origin

--- router/server.js
+++ router/server.js
@@ -5,6 +5,11 @@ const router = express.Router();
 const port = 3001;
 const bodyParser = require("body-parser");
 
+app.all('/', function(req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
