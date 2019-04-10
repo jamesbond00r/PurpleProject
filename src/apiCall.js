580a1f4f1b4407e403524759a2304117262c5948
jamesbond00r <jamesbond00r@gmail.com>
2019-04-10 16:45:36 +0000

    Make Axios Call to Server.js

--- src/apiCall.js
+++ src/apiCall.js
@@ -3,12 +3,47 @@ import React, { Component } from 'react';
 import axios from "axios";
 
 class Call extends React.Component {
+    state = {
+        error: null,
+        isLoaded: false,
+        items: []
+    };
+
+	componentDidMount(){
+    axios.get("http://34.204.52.29:3001/").then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            items: result.data
+          }).catch(function(error) {
+          console.log('What happened? ' + error.response.data)});
+    },
+        error => {
+            this.setState({
+            isLoaded: true,
+            error
+            });
+         }
+        );
+       }
+
     render(){
-     return(
-       <div>
-        <h1>Hello</h1>
-       </div>
-)}}
+     const { error, isLoaded, items } = this.state;
+     if (error) {
+        return <div>Error: {error.message}</div>;
+       }
+           else if (!isLoaded){  
+               return <div>Loading...</div>;
+      }
+           else {
+           return(
+              <div>
+               <div>
+              {items}
+                </div>
+            ))}
+         </div>
+);}}}
 
 export default Call;
 
