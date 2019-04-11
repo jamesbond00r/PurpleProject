4c5a2ee0ca824c08ac9612f04c6fb79031b089bf
jamesbond00r <jamesbond00r@gmail.com>
2019-04-11 01:49:20 +0000

    connect to database thanks alex

--- router/mySql.js
+++ router/mySql.js
@@ -1,23 +1,20 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host : '34.204.52.29',
-    port : '8888',
+    host :'34.204.52.29',
+    port : '3306',
+    user  : 'clarkr',
     password : 'VYnZhYXRpFgWakwv',
-    database : 'interview9',
-    schema: 'interview9'
+    database : 'interview9'
 
 
 });
 
     connection.connect(function(err){
-        if (err) throw err;
+        if (err) console.log(err);
+
+	console.log("Connected!");
      });
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
-   if (error) console.log(error)
-    throw error;
-   console.log('The solution is: ', results[0].solution);
-  });
 
 connection.end();
