b721d5beacf4dbc28c771d7f1d507c41a90991b7
jamesbond00r <jamesbond00r@gmail.com>
2019-04-13 03:09:44 +0000

    Add React forum that updates state

--- src/apis/Delete.js
+++ src/apis/Delete.js
@@ -5,22 +5,22 @@ import Button from 'react-bootstrap/Button';
 class Delete  extends React.Component {
 	constructor(props){
 	super(props);
-	this.state = {value:0}
+	this.state = {id:0}
 
 	this.handleChange = this.handleChange.bind(this);
 	this.handleSubmit = this.handleSubmit.bind(this);
 	}	
 
 	handleChange = event=>{
-	 this.setState({value: event.target.value});
+	 this.setState({id: event.target.value});
 	}
 		
 	
 
 	handleSubmit(event){
 	event.preventDefault();
-	alert(this.state.value)
-	axios.delete("http://34.204.52.29:3050/", {body: { value:this.state. value}}
+	alert(this.state.id)
+	axios.delete("http://34.204.52.29:3050/delete", {body: { id:this.state.id}}
 		)
 	   .then(res => {
             console.log(res);
@@ -37,7 +37,7 @@ class Delete  extends React.Component {
 	Person ID:
 	<input type="text" value={this.state.value}  onChange={this.handleChange} />
 	</label>
-	<button type="submit">Delete</button>
+	<Button variant="danger"  type="submit">Delete</Button>
 	</form>
 	</div>
 )}}
