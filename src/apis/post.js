0e4f0154f94fad9321e7f0132b5564583e58527b
jamesbond00r <jamesbond00r@gmail.com>
2019-04-13 21:27:48 +0000

    add fetch remove axios

--- src/apis/post.js
+++ src/apis/post.js
@@ -3,51 +3,54 @@ import axios from "axios";
 import Button from 'react-bootstrap/Button';
 
 class Post  extends React.Component {
-	constructor(props){
-	super(props);
-	this.state = {nameFirst:""}
+  constructor(props){
+  super(props);
+  this.state = {nameFirst:""}
 
-	this.handleChange = this.handleChange.bind(this);
-	this.handleSubmit = this.handleSubmit.bind(this);
-	}	
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  } 
 
-	handleChange = event=>{
-	 this.setState({nameFirst: event.target.value});
-	}
-		
-	
-
-	handleSubmit(event){
-	event.preventDefault();
-	 const user = {
-		name: this.state.name
-	};
-	alert(this.state.nameFirst)
-	axios.post(`http://34.204.52.29:3050/post/${user}`)
-	   .then(res => {
+  handleChange = event=>{
+   this.setState({nameFirst: event.target.value});
+	 
+ }
+  
+  handleSubmit(event){  
+  var params =`{name:${this.state.nameFirst}}`
+  var url = `http://34.204.52.29:3050/post/`
+  var requestOptions= {
+  method: 'post',
+  headers: {
+'Accept': 'application/json',
+'Content-type': 'application/json'},
+body: JSON.stringify(this.state.nameFirst)
+  };
+  event.preventDefault();
+  var encoded = window.encodeURI(url+requestOptions)
+  
+  fetch(encoded)
+     .then(res => {
             console.log(res);
-            console.log(res.data);
-	
+            
+  
          })
     }
 
-	render(){
+  render(){
          return(
-	<div>
+  <div>
         <form onSubmit={this.handleSubmit}>
-	
-	<label>
-	Person Name:
-	<input
-	 type="text"
-	 value={this.state.value}
-	 onChange={this.handleChange} />
-	</label>
-	<Button type="submit">Add/Post</Button>
-	</form>
-	</div>
+  <label>
+  Person name:
+  <input type="text" value={this.state.value}  onChange={this.handleChange} />
+  </label>
+  <Button   type="submit">Submit</Button>
+  </form>
+  </div>
 )}}
 
 
 
 export default Post;
+
